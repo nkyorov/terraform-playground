5dfabfadf89cab942626bde2817621e6ad9c0e26
Nikolay Kyorov <n.kyorov@gmail.com>
2022-02-02 19:14:57 +0200

    Initial commit

---
+++ azure/kubernetes-the-hard-way/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.94.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
+}
