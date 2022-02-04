5aecb8a1eef3ccd74a4aa1a20dc56efae6e0754e
Nikolay Kyorov <n.kyorov@gmail.com>
2022-02-04 11:37:17 +0200

    Add descriptions to all variables

--- azure/kubernetes-the-hard-way/variables.tf
+++ azure/kubernetes-the-hard-way/variables.tf
@@ -1,70 +1,87 @@
 variable "rg_name" {
   type = string
+  description = "All resources will be deployed here."
 }
 
 variable "rg_location" {
   type = string
+  description = "The Azure location of all resources."
 }
 
 variable "nsg_name" {
   type = string
+  description = "Name of the NSG attached to the subnet."
 }
 
 variable "vnet_address_spaces" {
   type = list(string)
+  description = "Address space of the VNET."
 }
 
 variable "subnet_prefixes" {
   type = list(string)
+  description = "List of subnet address spaces."
 }
 
 variable "subnet_names" {
   type = list(string)
+  description = "List of the subnet names to be created."
 }
 
 variable "vnet_name" {
   type = string
+  description = "Name of the virtual network."
 }
 
 variable "lb_name" {
   type = string
+  description = "Name of the Load Balancer."
 }
 
 variable "pip_lb_name" {
   type = string
+  description = "Name of the PIP attached to the LB."
 }
 
 variable "pip_lb_alloc" {
   type = string
+  description = "Allocation method for the PIP of the Load balancer."
 }
 
 variable "controllers_avail_set_name" {
   type = string
+  description = "Name of the availability set dedicated to the controllers."
 }
 
 variable "workers_avail_set_name" {
   type = string
+  description = "Name of the availability set dedicated to the workers."
 }
 
 variable "os_image" {
   type = map(string)
+  description = "The details of the Azure VM image."
 }
 
 variable "vm_size" {
   type = string
+  description = "Size of all VMs."
 }
 
 variable "controllers_count" {
   type = number
+  description = "Number of controllers to be deployed."
 }
 
 variable "workers_count" {
   type = number
+  description = "Number of workers to be deployed."
 }
 
 variable "tags" {
   type    = map(any)
   default = {}
+  description = "Tags to be added to all resources."
 }
 
 locals {
